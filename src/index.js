efc00ba3480e7b8090e2eaa3fced5338abca7f14
Artsiom Rak <ra44elo@gmail.com>
2019-03-14 21:05:19 +0300

    feat: add implementation for remainder 20% of tests

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 module.exports = function getZerosCount(number, base) {
-  
-  //? for decimnal base system
-  // let zeros = 0;
-  // for(let i = 5; i <= number; i*=5) {
-  //   zeros += Math.floor(number / i);
-  // }
-  // return zeros;
-
-  let arrOfSipmleNumbersOfBase = [];
+  let primaryFactorsArr = [];
+  let countedZerosArr = [];
   let flag = true;
   let i = 2;
   while(flag) {
@@ -15,20 +8,26 @@ module.exports = function getZerosCount(number, base) {
       flag == false;
       break;
     } else if(base % i == 0) {
-      arrOfSipmleNumbersOfBase.push(i);
+      primaryFactorsArr.push(i);
       base = base / i;
       continue;
     }
     i += 1;
   }
 
-  let p = arrOfSipmleNumbersOfBase.pop();
-  function z (number) {
+  function countZerosByPrimaryFactor(num, prFactItem) {
     let zeros = 0;
-    for(let i = p; i <= number; i *= p) {
-      zeros += Math.floor(number / i);
+    for(let i = prFactItem; i <= num; i *= prFactItem) {
+      zeros += Math.floor(num / i);
     }
     return zeros;
   }
-  return z(number);
+
+  for (let k = 0; k < primaryFactorsArr.length; k += 1) {
+    countedZerosArr.push(Math.floor(
+      countZerosByPrimaryFactor(number, primaryFactorsArr[k]) / primaryFactorsArr.filter(item => item === primaryFactorsArr[k]).length)
+    );
+  }
+
+  return Math.min.apply(null, countedZerosArr);
 }
