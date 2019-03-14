bc42768cc8bed7b85b173566a1cd6a2aa30ae389
Artsiom Rak <ra44elo@gmail.com>
2019-03-14 14:30:58 +0300

    add 80% of implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  
+  //? for decimnal base system
+  // let zeros = 0;
+  // for(let i = 5; i <= number; i*=5) {
+  //   zeros += Math.floor(number / i);
+  // }
+  // return zeros;
+
+  let arrOfSipmleNumbersOfBase = [];
+  let flag = true;
+  let i = 2;
+  while(flag) {
+    if(base / i < 1) {
+      flag == false;
+      break;
+    } else if(base % i == 0) {
+      arrOfSipmleNumbersOfBase.push(i);
+      base = base / i;
+      continue;
+    }
+    i += 1;
+  }
+
+  let p = arrOfSipmleNumbersOfBase.pop();
+  function z (number) {
+    let zeros = 0;
+    for(let i = p; i <= number; i *= p) {
+      zeros += Math.floor(number / i);
+    }
+    return zeros;
+  }
+  return z(number);
 }
